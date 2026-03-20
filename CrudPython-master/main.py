b9b1ce1412e310d7d33ef2efa48b371de5069a19
Jose_Kaue_Araujo_Alves <josekauearujo@gmail.com>
2026-03-20 19:30:53 -0300

    Update main.py

--- CrudPython-master/main.py
+++ CrudPython-master/main.py
@@ -1,40 +1,56 @@
 from db import Usuario, Session
 
+# # GET - Buscar usuários
+
 # with Session() as sessao:
-#    usuario = sessao.query(Usuario).filter_by(id=1).first()
-#    sessao.delete(usuario)
-#    sessao.commit()
+#     usuarios = sessao.query(Usuario).all()
+
+#     for usuario in usuarios:
+#         print(usuario.nome)
+#         print(usuario.email)
+#         print(usuario.idade)
+#         print("===========")
+
+
+# GET - Buscar usuário por id
+
+# with Session() as sessao:
+#     usuario = sessao.query(Usuario).filter_by(nome= "Rafael ").first()
+
+#     if usuario:
+#         print(usuario.nome)
+#         print(usuario.email)
+#         print(usuario.idade)
+
+
+# POST  Criar usuário
+# with Session() as sessao:
+#     while True:
+#      nome = input("Nome: ")
+#      email = input("Email: ")
+#      idade = input("Idade: ")
+ 
+#      novo_usuario = Usuario(nome=nome, email=email, idade=idade)
+#      sessao.add(novo_usuario)
+#      sessao.commit()
 
 
 
-#   usuario = sessao.query(Usuario).filter_by(id).first()
-# print(usuario.nome)
+
+# # PUT/PATCH - Atualizar usuário
+
+# with Session() as sessao:
+#     usuario = sessao.query(Usuario).filter_by(id=1).first()
+
+#     if usuario:
+#         usuario.nome = "Lucas"
+#         sessao.commit()
 
 
+# DELETE - Deletar usuário
+# with Session() as sessao:
+#     usuario = sessao.query(Usuario).filter_by(id=1).first()
 
-#  usuarios = sessao.query(Usuario).all()
-# for usuario in usuarios:
-#  print(usuario.nome)
-#  print(usuario.email)
-#  print(usuario.idade)
-#  print("===========")
-
-
-
-
-
-
-
-
-
-
-with Session() as sessao:
- while True:
-    nome = input("Nome: ")
-    email = input("Email: ")
-    idade = input("Idade: ")
-
-    novo_usuario= Usuario(nome= nome, email=email, idade=idade)
-    sessao.add(novo_usuario)
-    sessao.commit()
-    sessao.close()
+#     if usuario:
+#         sessao.delete(usuario)
+#         sessao.commit()
